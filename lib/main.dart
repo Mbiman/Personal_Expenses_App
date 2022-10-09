d6ff1bb4acc4bd2060ef98d9c99f6d911037702a
mbiman <juniormbi80@gmail.com>
2022-10-09 07:56:33 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: Column(
+        children: const [
+          Card(
+            child: Text('Chart!'),
+          ),
+          Card(
+            child: Text('List of transactions'),
+          )
+        ],
+      ),
+    );
+  }
+}
