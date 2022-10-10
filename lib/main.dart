d5fcfdd5693cfca7f90437acdc75e8844ce1b1e4
mbiman <juniormbi80@gmail.com>
2022-10-10 11:00:45 +0100

    dateFormatting using Intl package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_expenses/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,24 +21,84 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    final List<Transaction> transactions = [
+      Transaction(
+        id: 't1',
+        title: 'New Shoes',
+        amount: 15400,
+        date: DateTime.now(),
+      ),
+      Transaction(
+        id: 't2',
+        title: 'Weekly Groceries',
+        amount: 4500,
+        date: DateTime.now(),
+      )
+    ];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter App'),
       ),
       body: Column(
-        children: const [
-          SizedBox(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
             width: double.infinity,
             child: Card(
               elevation: 5,
               child: Text('Chart!'),
             ),
           ),
-          Card(
-            child: Text('List of transactions'),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      )),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        '₦${tx.amount}',
+                        style: GoogleFonts.ubuntu(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tx.title!,
+                          style: GoogleFonts.aBeeZee(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          DateFormat.yMMMd().format(tx.date!),
+                          style: GoogleFonts.aBeeZee(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           )
         ],
       ),
